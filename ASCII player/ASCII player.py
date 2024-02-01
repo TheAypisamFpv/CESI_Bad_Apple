fd2153c02396536d989e27474f00f242c184d705
the aypisam fpv <the.aypisam.64@gmail.com>
2024-02-01 12:27:03 +0100

    bug fix

--- ASCII player/ASCII player.py
+++ ASCII player/ASCII player.py
@@ -15,7 +15,7 @@ if file == "":
     frame_height = int(size.split("x")[0])
     frame_width = int(size.split("x")[1])
 
-    fps = int(properties.split("@")[1].removesuffix("fps"))
+    fps = int(properties.split("@")[1].removesuffix("fps").split(".")[0])
 
     print(f"please make sure all gathered information is correct:\n\tframe_nbr = {frame_nbr}\n\tframe_height = {frame_height}\n\tframe_width = {frame_width}\n\tfps = {fps}")
     input("Press enter to continue...")
