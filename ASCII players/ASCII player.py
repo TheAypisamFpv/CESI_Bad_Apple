4fb83542dfde16c22d34d001936e15cfd24d95ae
the aypisam fpv <the.aypisam.64@gmail.com>
2024-02-01 15:29:56 +0100

    fps bug fix

--- ASCII players/ASCII player.py
+++ ASCII players/ASCII player.py
@@ -13,7 +13,7 @@ size = properties.split("@")[0]
 frame_height = int(size.split("x")[1])
 frame_width = int(size.split("x")[0])
 
-fps = float(properties.split("@")[1].removesuffix("fps").replace(".", ","))
+fps = float(properties.split("@")[1].removesuffix("fps"))
 
 print(f"please make sure all gathered information is correct:\n\tframe_nbr = {frame_nbr}\n\tframe_height = {frame_height}\n\tframe_width = {frame_width}\n\tfps = {fps}")
 input("Press enter to continue...")
@@ -34,7 +34,7 @@ prev_i = 0
 start_time = time.time()
 dropped_frames = 0
 while i < frame_nbr:
-    i = int((time.time() - start_time)*fps)
+    i = round((time.time() - start_time)*fps)
     if i != prev_i:
         dropped_frames += i - (prev_i + 1)
 
