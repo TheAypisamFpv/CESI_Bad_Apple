764a6d0d1520ab8a1c403b02b7add0407d5d57de
the aypisam fpv <the.aypisam.64@gmail.com>
2024-03-11 09:28:31 +0100

    new .txt

--- video to ASCII/video_to_ascii.py
+++ video to ASCII/video_to_ascii.py
@@ -3,7 +3,24 @@ import numpy as np
 
 
 #-------PARAMETERS-------#
+default = '\033['
+reset = '\033[0m'
 
+colors = {
+    "30": [0,0,0],
+    "31": [128,0,0],
+    "32": [0,128,0],
+    "33": [128,128,0],
+    "34": [0,0,128],
+    "35": [128,0,128],
+    "36": [0,128,128],
+    "37": [192,192,192],
+    }
+
+# print the color table
+print("Color table :")
+for color in colors:
+    print(f"{default}{color}m{color} : {colors[color]}")
 
 VIDEO_PATH = "input video\\"
 VIDEO_PATH += input("Enter the name of the video you want to convert (must be in 'input video' folder):\n> ")
@@ -13,30 +30,10 @@ if not os.path.isfile(VIDEO_PATH):
 COLOR = False if input("Convert to grayscale Y/n :\n> ") == "" else True
 
 
-default = '\033['
-reset = '\033[0m'
 
-colors = {
-    "30": [0,0,0],
-    "31": [205,0,0],
-    "32": [0,205,0],
-    "33": [205,205,0],
-    "34": [0,0,238],
-    "35": [205,0,205],
-    "36": [0,205,205],
-    "37": [229,229,229],
-    "90": [127,127,127],
-    "91": [255,0,0],
-    "92": [0,255,0],
-    "93": [255,255,0],
-    "94": [92,92,255],
-    "95": [255,0,255],
-    "96": [0,255,255],
-    "97": [255,255,255]
-    }
 
 def find_color(pixel:list[int]):
-    closest = "30"
+    closest = ""
     closest_dist = 255*3
     for color in colors:
         dist = 0
@@ -48,7 +45,7 @@ def find_color(pixel:list[int]):
     return closest
 
 inputs = input("Resize factor (default 10) (ASCII video will have <RESIZE_FACTOR> times smaller dimension):\n> ")
-RESIZE_FACTOR = 10 if inputs == "" else round(inputs)
+RESIZE_FACTOR = 10 if inputs == "" else int(inputs)
 """Determines the number of pixels par character
 ---
 default 25 : 5*5px to 1chr
@@ -58,7 +55,7 @@ default 25 : 5*5px to 1chr
 CHARS = np.asarray(list(' ._,:;-÷+=*?%S#@'))
 """ ._,:;-÷+=*?%S#@"""
 
-INVERTED = False if input("Invert the video (default white = char, black = no char) (default False) y/n(leave empty) :\n> ") == "" else True
+INVERTED = False if input("Invert the video (default white = char, black = no char) (default False) y/N :\n> ") == "" else True
 
 if INVERTED:
     CHARS = CHARS[::-1]
@@ -80,7 +77,7 @@ cap = cv2.VideoCapture(VIDEO_PATH)
 ascii_frames = []
 i = 0
 
-width, height, fps = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)), cap.get(cv2.CAP_PROP_FPS)
+width, height, fps = round(cap.get(cv2.CAP_PROP_FRAME_WIDTH)//RESIZE_FACTOR), round(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)//(RESIZE_FACTOR*2)), cap.get(cv2.CAP_PROP_FPS)
 
 print(f"Converting '{VIDEO_PATH}' to ASCII...")
 frame_nbr = 0
@@ -97,7 +94,7 @@ while cap.isOpened():
         if not COLOR:
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        frame = cv2.resize(frame, (frame.shape[1]//RESIZE_FACTOR, frame.shape[0]//(RESIZE_FACTOR*2)), interpolation=cv2.INTER_AREA)
+        frame = cv2.resize(frame, (round(frame.shape[1]//RESIZE_FACTOR), round(frame.shape[0]//(RESIZE_FACTOR*2))), interpolation=cv2.INTER_AREA)
 
         frame_nbr += 1
         
@@ -106,7 +103,7 @@ while cap.isOpened():
             for pixel in row:
                 if COLOR:
                     rgb_pixel = find_color(pixel)
-                    ascii_frames.append(f'{default}{rgb_pixel}m')
+                    ascii_frames.append(f'\033[{rgb_pixel}m')
                     #confert the pixel to WB for luminosity conversion
                     pixel = (pixel[0] + pixel[1] + pixel[2]) / 3
                     
@@ -122,11 +119,15 @@ while cap.isOpened():
 
 pgbar.close()
 
-file_name = "ASCII videos\\" + VIDEO_PATH.split("\\")[-1].split(".")[0] + f"_{width}x{height}@{fps}fps_{frame_nbr}_{STORAGE_METHOD}_{'normal' if not INVERTED else 'inverted'}.txt"
+file_type = "colored" if COLOR else "grayscale"
+if not COLOR:
+    file_type += "-inverted" if INVERTED else ""
+
+file_name = "ASCII videos\\" + VIDEO_PATH.split("\\")[-1].split(".")[0] + f"_{width}x{height}@{fps}fps_{frame_nbr}_{STORAGE_METHOD}_{file_type}.txt"
 #save the ASCII frames in a file
 with open(file_name, "w") as f:
     f.write(''.join(ascii_frames))
     
 print(f"ASCII video saved in '{file_name}'")
 #print the ascii video dimensions
-print(f"ASCII video settings : {width}x{height}@{fps}fps_{frame_nbr}_{STORAGE_METHOD}")
+print(f"ASCII video settings : {width//RESIZE_FACTOR}x{height//(RESIZE_FACTOR*2)}@{fps}fps_{frame_nbr}_{STORAGE_METHOD}")
